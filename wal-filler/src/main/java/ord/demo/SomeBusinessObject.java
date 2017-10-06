95e66473046f2cd75ca9bc7f1ef3072784b056e4
dpavlov <dpavlov@gridgain.com>
2017-10-06 20:52:48 +0300

    libs added

---
+++ wal-filler/src/main/java/ord/demo/SomeBusinessObject.java
@@ -0,0 +1,33 @@
+package ord.demo;
+
+/**
+ * Emulates business object
+ */
+public class SomeBusinessObject {
+    private String value;
+    private final int index;
+
+    public SomeBusinessObject(String value, int index) {
+        this.value = value;
+        this.index = index;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    @Override public String toString() {
+        return "SomeBusinessObject{" +
+            "value='" + value + '\'' +
+            ", index=" + index +
+            '}';
+    }
+}
